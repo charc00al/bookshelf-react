050ad5871d2724ff9323ea930e791493ac366ca9
charcOOal <sarka-stranska@seznam.cz>
2025-09-14 20:22:23 +0200

    feat: bookshelf  homepage demo version in process

--- src/App.jsx
+++ src/App.jsx
@@ -1,10 +1,22 @@
+// components
+import Header from "./components/Header";
+import Book from "./components/Book";
 
+// styles
+import styles from './styles/Bookshelf.module.css';
+
+// App
 function App() {
-
   return (
-<h1>
-  Hello world
-</h1>
+    <div>
+      <Header/>
+      <section className={styles.bookshelf}>
+        <Book/>
+        <Book/>
+        <Book/>
+      </section>
+
+    </div>
   )
 }
 export default App
---
+++ src/components/Header.jsx
@@ -0,0 +1,6 @@
+      
+function Header() {
+    return <h1>Bookshelf</h1>
+}
+
+export default Header
